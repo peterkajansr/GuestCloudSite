f0aeaf8b60b8b1215f14fa12f04eb0a2afd0eb66
Peter Kajan <peto.kajan@gmail.com>
2014-08-16 16:40:43 +0200

    GuestFlow page, urls refactor, i18l, templates structure

--- crm/urls.py
+++ crm/urls.py
@@ -6,5 +6,8 @@ urlpatterns = patterns('',
     url(r'^profiles/$', views.profile_redirect, name='profile_redirect'),
     url(r'^accounts/', include('django.contrib.auth.urls', namespace='auth')),
     url(r'^register/', views.register, name='register'),
-    url(r'^p/(?P<username>.*)/$', views.profile_home, name='profile_home'),
+    url(r'^p/(?P<username>[^/]*)/', include([
+        url(r'^$', views.profile_home, name='profile_home'),
+        url(r'^guestflow/$', views.guestflow, name='guestflow'),
+    ])), 
 )
--- crm/views.py
+++ crm/views.py
@@ -1,27 +1,39 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.http.response import HttpResponseRedirect, Http404
 from django.core.urlresolvers import reverse
-from django.contrib.auth.views import redirect_to_login
 from crm.forms import UserCreationForm
 # from firebasein.firebase import Firebase
 
+def own_profile(function):
+    """ ensures that username parsed from url matches 
+        username of the logged user """
+    def wrapped(request, *args, **kwargs):
+        username = kwargs.pop('username')
+        if request.user.username != username:
+            raise Http404()  
+        return function(request, *args, **kwargs)
+    wrapped.__name__ = function.__name__
+    return wrapped
 
 
 @login_required
-def profile_home(request, username):
-    if request.user.username != username:
-        raise Http404()
-    
-#     firebase = Firebase('https://guestflow.firebaseio.com/')
-#     print firebase.child('users')
+@own_profile
+def profile_home(request):
     return render(request, 'crm/profile_home.html', {})
+
+
+@login_required
+@own_profile
+def guestflow(request):
+    return render(request, 'crm/guestflow.html', {})
+
 
 @login_required
 def profile_redirect(request):
     return HttpResponseRedirect(reverse('crm:profile_home', 
                                  kwargs={'username': request.user.username}))
+    
 def register(request):
     form = UserCreationForm()
     if request.method == 'POST':
